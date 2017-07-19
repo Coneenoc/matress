a2730ec0e4eff9ca303ff1e78e7f60add8523486
Coneenoc <alex.junior_@mail.ru>
2017-07-19 13:11:41 +0600

    Added bg and style sheet

--- index.php
+++ index.php
@@ -3,15 +3,14 @@
 		<title></title>
 		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css?ver=<?=rand(763324,7879872987)?>">
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<div>
 			<script type="text/javascript">
 				$('documnet').ready(function() {
-
-					var $animation_elements = $('.col-sm-4');
-			        var $window = $(window);
-
+					var $animation_elements = $('.an');
+			var $window = $(window);
 			function check_if_in_view() {
 					var window_height = $window.height();
 					var window_top_position = $window.scrollTop();
@@ -27,120 +26,55 @@
 					
 					//check to see if this current container is within viewport
 					if ((element_bottom_position >= window_top_position) &&
-					(element_top_position <= window_bottom_position) && 
+					(element_top_position <= window_bottom_position) &&
 					($element.hasClass($effect)==false)) {
-			          
-			          setTimeout(function() {
+			
+			setTimeout(function() {
 						$element.addClass($effect);
 								}, $timeout);
 					
 					
 					} else {
-					 //$element.removeClass($effect);
+					//$element.removeClass($effect);
 					}
 					});
 				}
-
 				$window.on('scroll resize', check_if_in_view);
-			    $window.trigger('scroll');
-
+			$window.trigger('scroll');
 				});
 			</script>
 		</div>
 	</head>
 	<body>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<div>
+		<div class="container-fluid" style="background: url('img/bg.jpg') center center no-repeat;height: 300px;">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-4" data-time='300' data-effect='bounceIn animated'>sample text 1</div>
-					<div class="col-sm-4" data-time='1200' data-effect='bounceIn animated'>sample text 2</div>
-					<div class="col-sm-4" data-time='1800' data-effect='bounceIn animated'>sample text 3</div>
+					cvxcvxcv sdfd fg df
 				</div>
 			</div>
 		</div>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
+		
+		<div>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="an" data-time='300' data-effect='fadeInLeft animated'>test sampl 1
+                            <div class="an" data-time='1500' data-effect='fadeInRight animated'> 
+                            i fade in form the right
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas culpa quibusdam id consequatur non maxime tempora? Quos, adipisci doloremque atque non earum fuga, vero nemo cupiditate corrupti ut ea labore.</p>
+
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="an" data-time='300' data-effect='fadeInLeft animated'>test sampl 1</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="an" data-time='300' data-effect='fadeInLeft animated'>test sampl 1</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
 	</body>
 </html>
